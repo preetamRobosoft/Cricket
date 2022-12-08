93301359ab1fb616c116a80e4df627ef4d758eab
preetamRobosoft <preetam.godase@robosoftin.com>
2022-12-08 10:35:46 +0530

    Register view controller

--- Cricket/Controller/RegisterViewController.swift
+++ Cricket/Controller/RegisterViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
-
+    
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var proceedButton: UIButton!
     @IBOutlet weak var maleGender: RadioButton!
@@ -16,8 +16,12 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var customProfileView: ProfilePhotoImageView!
     @IBOutlet weak var triangleView: TriangleView!
     @IBOutlet weak var profileImage: CustomImageView!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var phoneTextField: UITextField!
+    @IBOutlet weak var cityTextField: UITextField!
     
-    //let registerViewModel = RegisterViewModel()
+    let registerVM = RegistrationViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,24 +33,96 @@ class RegisterViewController: UIViewController {
     func configureButtons() {
         
         _ = self.proceedButton.applyGradient( colours: [ #colorLiteral(red: 1, green: 0.7294117647, blue: 0.5490196078, alpha: 1), #colorLiteral(red: 0.9960784314, green: 0.3607843137, blue: 0.4156862745, alpha: 1)], cornerRadius: 4)
-        //registerViewModel.assignGenderButtons(buttons: [maleGender, femaleGender])
     }
-
+    
     @IBAction func genderButtonTapped(_ sender: RadioButton) {
         sender.changeButtonsStatus(sender: sender, buttons: [maleGender, femaleGender])
+        registerVM.currentUser.gender = sender.title(for: .normal)!.trimmingCharacters(in: .whitespaces)
     }
     
     @IBAction func backButtonTapped(_ sender: Any) {
-
-            navigationController?.popViewController(animated: true)
-
+        
+        navigationController?.popViewController(animated: true)
+        
+    }
+    
+    @IBAction func onClickCamera(_ sender: Any) {
+        let vc = UIImagePickerController()
+        vc.sourceType = .photoLibrary
+        vc.delegate = self
+        vc.allowsEditing = true
+        present(vc, animated: true)
+    }
+    
+    
+    @IBAction func onRegisterTapped(_ sender: Any) {
+        
+        guard areValidFields() else { return }
+        let passwordVC = self.storyboard?.instantiateViewController(identifier: "PasswordViewController") as! PasswordViewController
+        self.navigationController?.pushViewController(passwordVC, animated: true)
+        passwordVC.isForgotPassword = false
+        passwordVC.currentViewModel = self.registerVM
+        
+    }
+    
+    func areValidFields() -> Bool {
+        guard let name = nameTextField.text else {
+            //TODO: add a warning label
+            //            nameWarningLabel.Text = "Username can't be empty"
+            print("empty username")
+            return false
         }
-
-        @IBAction func onRegisterTapped(_ sender: Any) {
-
-            let registerVC = storyboard?.instantiateViewController(identifier: "RegisterViewController") as! RegisterViewController
-
-            navigationController?.pushViewController(registerVC, animated: true)
-
+        
+        guard let email = emailTextField.text else {
+            //TODO: add a warning label
+            //            nameWarningLabel.Text = "Username can't be empty"
+            print("empty email")
+            return false
         }
+        
+        guard isValidEmail(email: email) else {
+            //TODO: add a warning label
+            //            nameWarningLabel.Text = "Username can't be empty"
+            print("invalid email")
+            return false
+        }
+        guard let phone = phoneTextField.text else {
+            //TODO: add a warning label
+            //            nameWarningLabel.Text = "Username can't be empty"
+            print("empty phone")
+            return false
+        }
+        let newPhone = phone.trimmingCharacters(in: .whitespaces)
+        
+//        TODO
+//        guard validateMobileNumber(number: newPhone)  else {
+//            //TODO: add a warning label
+//            //            nameWarningLabel.Text = "Username can't be empty"
+//            print("invalid phone")
+//            return false
+//        }
+        
+        registerVM.currentUser.username = name
+        registerVM.currentUser.email = email
+        registerVM.currentUser.phoneNumber = newPhone
+        print(newPhone)
+        
+        return true
+    }
+    
+}
+
+
+extension RegisterViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let fetchedImage = info[UIImagePickerController.InfoKey(rawValue: "UIImagePickerControllerEditedImage")] as? UIImage
+        profileImage.image = fetchedImage
+        picker.dismiss(animated: true, completion: nil)
+
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
 }
